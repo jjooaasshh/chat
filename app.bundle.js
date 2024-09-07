8e1e2ddf97245527d95111a4b0d43c0a971ed5dd
jjooaasshh <j5o6a2s7h4@outlook.com>
2024-09-07 15:25:06 +0800

    Add files via upload

--- app.bundle.js
+++ app.bundle.js
@@ -28937,7 +28937,7 @@
           r = t.addr;
         return e.createElement(
           pn,
-          { container: !0, direction: "row", spacing: 2 },
+          { container: !0, direction: "row", spacing: 1 },
           e.createElement(
             pn,
             { item: !0 },
