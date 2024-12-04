b04d4a0f29bda51cea09bdd3a9e0c141c21a17c0
jjooaasshh <j5o6a2s7h4@outlook.com>
2024-12-04 11:19:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-window.addEventListener("load", () => {
-    console.log("页面已加载，开始执行脚本...");
 
-    // 使用 setTimeout 延迟执行，等待页面完全加载
-    setTimeout(() => {
         console.log("开始获取目标元素...");
         const targetElement1 = document.querySelector(".SlideUp");
         const targetElement2 = document.getElementById("Tips_Warn1");
@@ -22,5 +18,3 @@ window.addEventListener("load", () => {
         } else {
             console.log("未找到 #Tips_Warn1 元素");
         }
-    }, 10000); // 延迟 3 秒，时间可以根据需要调整
-});
